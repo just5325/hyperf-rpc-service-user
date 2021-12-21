9b50980eaad164e57727c95d7eb7e8cd8483a675
hcg <532508307@qq.com>
2021-12-21 11:19:17 +0800

    使用服务中心consul 对应文档：https://www.jianshu.com/p/591ff69fe5e7

--- app/JsonRpc/UserService.php
+++ app/JsonRpc/UserService.php
@@ -7,7 +7,7 @@ use Hyperf\Contract\ConfigInterface;
 
 /**
  * 注意，如希望通过服务中心来管理服务，需在注解内增加 publishTo 属性
- * @RpcService(name="UserService", protocol="jsonrpc-http", server="jsonrpc-http")
+ * @RpcService(name="UserService", protocol="jsonrpc-http", server="jsonrpc-http", publishTo="consul")
  */
 class UserService implements UserServiceInterface
 {
---
+++ config/autoload/services.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    'enable' => [
+        // 开启服务发现
+        'discovery' => true,
+        // 开启服务注册
+        'register' => true,
+    ],
+    // 服务消费者相关配置
+    'consumers' => [],
+    // 服务提供者相关配置
+    'providers' => [],
+    // 服务驱动相关配置
+    'drivers' => [
+        'consul' => [
+            'uri' => 'http://172.17.0.3:8500',
+            'token' => '',
+            'check' => [
+                'deregister_critical_service_after' => '90m',
+                'interval' => '1s',
+            ],
+        ],
+    ],
+];
